baaaf7e60c080b3f170c377305ca8f5bba04f872
swayli94 <swayli94@gmail.com>
2020-01-15 11:11:07 +0800

    debug

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 with open('Readme.md') as f:
       long_description = f.read()
 
-setup(name='cst_modeling',
+setup(name='cst_modeling3d',
       version='0.1',
       description='This is the module of surface/airfoil modeling',
       long_description=long_description,
       keywords='CST modeling',
-      download_url='https://github.com/swayli94/cst-modeling/',
+      download_url='https://github.com/swayli94/cst-modeling3d/',
       license='MIT',
       author='Runze LI',
       author_email='swayli94@gmail.com',

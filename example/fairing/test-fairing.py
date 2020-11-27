699055a325df70ade96f8082c7f290d19c4cda68
swayli94 <swayli94@gmail.com>
2020-11-27 17:53:25 +0800

    fairing tutorial

--- example/fairing/test-fairing.py
+++ example/fairing/test-fairing.py
@@ -1,72 +1,67 @@
 
 
 import numpy as np
-from cst_modeling.foil import cst_curve
 from cst_modeling.surface import BasicSurface
 
+from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
 
-
-    fairing = BasicSurface(n_sec=8, name='Fairing', nn=51, ns=51, project=False)
+    fairing = BasicSurface(n_sec=8, name='Fairing', nn=51, ns=101, project=False)
     fairing.read_setting('Fairing.txt')
 
-    #* Fairing end curves
-    with open('..\\fuselage\\tube-section.dat', 'r') as f:
+
+    # Read raw data
+    with open('..\\fuselage\\tube-curve.dat', 'r') as f:
+        X_end = []
         Y_end = []
-        Z_end = []
         lines = f.readlines()
         for i in range(len(lines)-2):
-            if i >= 102 and i<= 152:
-                line = lines[i].split()
-                Y_end.append(float(line[0]))
-                Z_end.append(float(line[1]))
+            line = lines[i+2].split()
+            X_end.append(float(line[0]))
+            Y_end.append(float(line[1]))
 
-        Y_end = np.array(Y_end) - Y_end[0]
-        Z_end = np.array(Z_end) - Z_end[0]
+    X_end = np.array(X_end)
+    Y_end = np.array(Y_end)
 
     for i in range(fairing.n_sec):
-        fairing.secs[i].xx = Y_end.copy()
-        fairing.secs[i].yy = Z_end.copy()
+        fairing.secs[i].xx = X_end.copy()
+        fairing.secs[i].yy = Y_end.copy()
 
-    #* Inner curves
 
-    # Increment curve of yy
-    n_cst = 5
-    nx  = Y_end.shape[0]
-    cc  = Y_end[-1]
-    xx  = Y_end.copy()/cc
-    
-    cst = np.zeros(n_cst)
-    cst[0] = 0.0
-    cst[1] = 0.8
-    cst[2] = 1.0
-    cst[3] = 1.0
+    #* Control section curve
+    X0 = [X_end[-1], -2.30, -1.80, -0.80, X_end[0]]
+    Y0 = [0.0,        0.40,  0.55,  0.10, 0.0]
+    curve = CubicSpline(X0, Y0, bc_type=((1,0.0), (1,0.0)))
 
-    _, y_i = cst_curve(nx, cst, x=xx)
-    fairing.secs[2].yy += np.flip(y_i)*np.abs(cc)
-    fairing.secs[3].yy += np.flip(y_i)*np.abs(cc)
-    fairing.secs[4].yy += np.flip(y_i)*np.abs(cc)
-    fairing.secs[5].yy += np.flip(y_i)*np.abs(cc)
+    y_increment = curve(X_end)
 
-    '''
-    plt.plot(Y_end, Z_end, 'b')
-    plt.plot(Y_end, fairing.secs[2].yy, 'r')
-    plt.axis('equal')
-    plt.show()
-    '''
+    if True:
+        #! Plot curves on screen
+        plt.plot(X_end, Y_end, 'r')
+        plt.plot(X0, Y0, 'go')
+        plt.plot(X_end, y_increment, 'g')
+        plt.plot(X_end, Y_end+y_increment, 'b')
+        plt.axis('equal')
+        plt.legend(['baseline curve', 'control points', 'incremental curve', 'final curve'])
+        plt.show()
+
+    fairing.secs[2].yy += y_increment
+    fairing.secs[3].yy += y_increment
+    fairing.secs[4].yy += y_increment*1.2
+    fairing.secs[5].yy += y_increment*1.2
+
 
     #* Build geometry
-    fairing.geo()
 
+    fairing.geo()
+    
     fairing.smooth(1, 3, smooth0=True, smooth1=True)
     fairing.smooth(5, 7, smooth0=True, smooth1=True)
 
     fairing.flip(axis='+Z +Y')
-
-    fairing.translate(dX=15.0, dY=0.0, dZ=2.6)
 
     fairing.output_tecplot(fname='fairing.dat')
 
